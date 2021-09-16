ebfdd493b627609709e43ebc855c6c7906c7cf9a
luisdramirez <ldr285@nyu.edu>
2021-09-16 10:45:34 -0400

    updated variable names and included hints

--- training_goal1.m
+++ training_goal1.m
@@ -10,35 +10,35 @@ commandwindow; % Forces cursor to command window
 Screen('Preference', 'SkipSyncTests',1); % 1 if running on my laptop.
 
 % Grab date
-t.theData = datestr(now, 'yyyymmdd'); % Grab today's date
-t.timeStamp = datestr(now,'HHMM'); % Grab current time
+t.the_date = datestr(now, 'yyyymmdd'); % Grab today's date
+t.the_time = datestr(now,'HHMM'); % Grab current time
 
 % Generate unique seed for random number generator
-t.mySeed = sum(100*clock);
-rng(t.mySeed)
+t.my_rng_seed = sum(100*clock);
+rng(t.my_rng_seed)
 
 % Set discreen_sizeories
-expDir =  pwd; % Set the experiment discreen_sizeory to the current path, grabbed by 'pwd'
-dataDir = 'raw_data';
+exp_dir =  pwd; % Set the experiment discreen_sizeory to the current path, grabbed by 'pwd'
+data_dir = 'raw_data';
 
 %%% Initialize Input %%%
 % Check which device number the keyboard is assigned to
-deviceNumber = 0;
-[keyBoardIndices, productNames, ~] = GetKeyboardIndices;
+device_number = 0;
+[keyboard_indices, product_names, ~] = GetKeyboardIndices;
 
 %deviceString = 'Wired USB Keyboard'; % testing room 207
 % deviceString = 'Apple Inc. Apple Keyboard'; % testing room 304
 % deviceString = 'USB-HID Keyboard';  % desk keyboard
-deviceString = 'Apple Internal Keyboard / Trackpad'; %my laptop
+device_string = 'Apple Internal Keyboard / Trackpad'; %my laptop
 
-for i = 1:length(productNames)
-    if strcmp(productNames{i}, deviceString)
-        deviceNumber = keyBoardIndices(i);
+for i = 1:length(product_names)
+    if strcmp(product_names{i}, device_string)
+        device_number = keyboard_indices(i);
         break;
     end
 end
 
-if deviceNumber == 0
+if device_number == 0
     error('No device by that name was detected');
 end
 
@@ -50,24 +50,24 @@ keyPressNumbers = [KbName('LeftArrow') KbName('RightArrow')];
 
 % Grab display parameters
 screens = Screen('Screens'); % Grab the available screens
-useScreen = max(screens); % If there are two or more displays, this will grab the most external display.
-p.screenWidthPix = Screen('rect', useScreen); % Grab the pixel resolution dimensions for the screen to be used
+use_screen = max(screens); % If there are two or more displays, this will grab the most external display.
+p.screen_size_pixels = Screen('rect', use_screen); % Grab the pixel resolution dimensions for the screen to be used
 
 % Set display width
-p.screenWidth = 28.6; % in centimeters; This is usually measured manually
+p.screen_width_cm = 28.6; % in centimeters; This is usually measured manually
 % Set viewing distance
-p.viewDistance = 57; % in centimeters; This is usually measured manually
+p.view_distance_cm = 57; % in centimeters; This is usually measured manually
 % Calculating visual angle of chosen display
-p.visAngle = (2*atan2(p.screenWidth/2,p.viewDistance))*(180/pi);
-p.pixels_per_degree = round(p.screenWidthPix(3)/p.visAngle); % Pixels per degree of visual angle
+p.visual_angle_degrees = (2*atan2(p.screen_width_cm/2,p.view_distance_cm))*(180/pi);
+p.pixels_per_degree = round(p.screen_size_pixels(3)/p.visual_angle_degrees); % Pixels per degree of visual angle
 
 % Set fixation dot size
-p.fixation = round(0.085*p.pixels_per_degree); % Calculate size of fixation in pixels
+p.fixation_size_pixels = round(0.085*p.pixels_per_degree); % Calculate size of fixation in pixels
 
 % Define any colors here
 gray = 128; white = 255; black = 0;
 green = [0 255 0]; red = [255 0 0];
-p.fixationColor = black;
+p.fixation_color = black;
 
 %% Parameters
 % % Fixation
@@ -84,44 +84,24 @@ p.fixationColor = black;
 % Grating Parameters
 
 % Size
-p.stimulusSz = round(4*p.pixels_per_degree);
+p.grating_size = round(4*p.pixels_per_degree);
 
 %  Contrast
-p.initContrast = 0.06; % initial contrast for target gratings (michelson contrast)
+p.grating_contrast = 0.06; % initial contrast for target gratings (michelson contrast)
 
 % Orientation
-p.gratingOrientation = 0;
+p.grating_orientation = 0;
 
 % Spatial frequency
 spatialFreq = 2;
-p.gratingSF = p.stimulusSz/p.pixels_per_degree * spatialFreq;
+p.grating_spatial_frequency = p.grating_size/p.pixels_per_degree * spatialFreq;
 
 % Phase
-p.gratingPhase = 360;
-
-%% Create Stimuli
-
-% make mask to create circle for the center grating
-[x,y] = meshgrid((-p.stimulusSz/2):(p.stimulusSz/2)-1, (-p.stimulusSz/2):(p.stimulusSz/2)-1);
-eccen = sqrt((x).^2+(y).^2); 	% calculate eccentricity of each point in grid relative to center of 2D image
-targetGaussian = zeros(p.stimulusSz); targetGaussian(eccen <= (p.stimulusSz/2)) = 1;
-
-% Make transparency mask for aplha blending the two images
-targetTransparencyMask = zeros(p.stimulusSz); targetTransparencyMask(eccen <= ((p.stimulusSz)/2)) = 255;
-
-% make grating
-[Xt,Yt] = meshgrid(0:(p.stimulusSz-1), 0:(p.stimulusSz-1));
-target = (sin(p.gratingSF*2*pi/p.stimulusSz*(Xt.*sin(p.gratingOrientation*(pi/180))+Yt.*cos(p.gratingOrientation*(pi/180)))-p.gratingPhase));
-% targetGrating= (target .* targetGaussian);
-
-% Make gratings textures
-targetGratings = NaN(p.stimulusSz, p.stimulusSz);
-% target = (sin(p.freq*2*pi/p.stimulusSz*(Xt.*sin(p.gratingOrientation*(pi/180))+Yt.*cos(p.gratingOrientation*(pi/180)))-p.targPhase));
-targetGratings(:,:) = (target .* targetGaussian);
+p.grating_spatial_phase = 360;
 
 %% WINDOW SETUP
 % Open window and where to display center targets.
-[window,screen_size] = Screen('OpenWindow', useScreen, gray,[],[],[],[],16);
+[window, screen_size] = Screen('OpenWindow', use_screen, gray,[],[],[],[],16);
 OriginalCLUT = Screen('ReadNormalizedGammaTable', window);
 %     load('linearizedCLUT.mat'); % testingR
 %     Screen('LoadNormalizedGammaTable', window, linearizedCLUT); %testingR
@@ -139,44 +119,52 @@ Screen('TextStyle', window, 1);
 Screen('TextSize', window, 16);
 t.ifi = Screen('GetFlipInterval',window); % grab screen refresh rate
 
-% p.degPerFrame = p.degPerSec * t.ifi;
-% p.degPerFrameGabor = sind(p.degTilt) .* p.degPerFrame;
+%% Create Stimuli
 
+% Create circular filter for textures
+[cartesian_x_coordinates,cartesian_y_coordinates] = meshgrid((-p.grating_size/2):(p.grating_size/2)-1, (-p.grating_size/2):(p.grating_size/2)-1);
+eccentricity_cartesian_coordinates = sqrt((cartesian_x_coordinates).^2+(cartesian_y_coordinates).^2); 	% calculate eccentricity of each point in grid relative to center of 2D image
+
+gaussian_filter = zeros(p.grating_size); 
+gaussian_filter(eccentricity_cartesian_coordinates <= (p.grating_size/2)) = 1;
+
+% Make grating texture grid
+[Xt,Yt] = meshgrid(0:(p.grating_size-1), 0:(p.grating_size-1));
+grating_texture_unfiltered = (sin(p.grating_spatial_frequency*2*pi/p.grating_size*(Xt.*sin(p.grating_orientation*(pi/180))+Yt.*cos(p.grating_orientation*(pi/180)))-p.grating_spatial_phase));
+
+% Apply filter to grating texture and convert texture to rgb space
+
+for n_contrast_level = 1:numel(p.grating_contrast)
+    temp_grating_texture = (grating_texture_unfiltered .* gaussian_filter); % apply gaussian filter
+    % apply contrast level and convert texture to rgb space -Luis
+    % make drawable texture and store -Luis
+end
 
 %% Draw fixation
-PsychHID('KbQueueCreate', deviceNumber);
-PsychHID('KbQueueStart', deviceNumber);
+PsychHID('KbQueueCreate', device_number);
+PsychHID('KbQueueStart', device_number);
 escaped = 0;
-
 
 % grating_coordinate_X = centerX;
 % grating_coordinate_Y = centerY;
 fixation_coordinate_X = centerX;
 fixation_coordinate_Y = centerY;
 update_coordinate = 1;
-update_speed = 50;
+update_size = 50;
 
 %moving stimulus vertically and horizontally
 while 1
-      
-    %--------------------%
-    %       Grating Update       %
-    %--------------------%
-    % Make gratings textures
-    targetGratings = NaN(p.targSize, p.targSize);
-    target = (sin(p.freq*2*pi/p.targSize*(Xt.*sin(p.orientation*(pi/180))+Yt.*cos(p.orientation*(pi/180)))-p.targPhase));
-    targetGratings(:,:) = (target .* targetGaussian);
-    gratingTexture(:,:,1) = targetGratings*p.targContrast;
-    gratingTexture(:,:,2) = targetTransparencyMask;
-    grating_patch = CenterRectOnPoint([0 0 p.stimulusSz p.stimulusSz], fixation_coordinate_X, fixation_coordinate_Y);
+    grating_patch = CenterRectOnPoint([0 0 p.grating_size p.grating_size], fixation_coordinate_X, fixation_coordinate_Y);
+    fixation_patch = CenterRectOnPoint([0 0 stim.fixationPx stim.fixationPx], fixation_coordinate_X, fixation_coordinate_Y); %defining size and location of fixation patch
     
-%     fixation_patch = CenterRectOnPoint([0 0 stim.fixationPx stim.fixationPx], fixation_coordinate_X, fixation_coordinate_Y); %defining size and location of stim patch
+    
+    % Draw texture  -Luis
     
     % Draw Fixation
-    Screen('FillOval', window, white, grating_patch)
+    Screen('FillOval', window, white, fixation_patch) 
     Screen('Flip', window);
     
-    [pressed, firstPress] = PsychHID('KbQueueCheck', deviceNumber);
+    [pressed, firstPress] = PsychHID('KbQueueCheck', device_number);
     if pressed == 1
         whichPress = find(firstPress);
         if any(ismember(whichPress, KbName('ESCAPE')))
@@ -184,16 +172,16 @@ while 1
             error('User exited program');
             break;
         elseif any(ismember(whichPress, KbName('UpArrow')))
-            fixation_coordinate_Y = fixation_coordinate_Y - update_speed;
+            fixation_coordinate_Y = fixation_coordinate_Y - update_size;
             
         elseif any(ismember(whichPress, KbName('DownArrow')))
-            fixation_coordinate_Y = fixation_coordinate_Y + update_speed;
+            fixation_coordinate_Y = fixation_coordinate_Y + update_size;
         
         elseif any(ismember(whichPress, KbName('LeftArrow')))
-            fixation_coordinate_X = fixation_coordinate_X - update_speed;
+            fixation_coordinate_X = fixation_coordinate_X - update_size;
         
         elseif any(ismember(whichPress, KbName('RightArrow')))
-            fixation_coordinate_X = fixation_coordinate_X + update_speed;
+            fixation_coordinate_X = fixation_coordinate_X + update_size;
             
         end
     end
@@ -202,8 +190,8 @@ while 1
 
 end
 
-PsychHID('KbQueueStop', deviceNumber);
-PsychHID('KbQueueFlush', deviceNumber);
+PsychHID('KbQueueStop', device_number);
+PsychHID('KbQueueFlush', device_number);
 
 
 
